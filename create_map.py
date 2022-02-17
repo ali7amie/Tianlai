112c9bfc7b8b6078b7868e29aa6593fff04ad448
ali7amie <92475780+ali7amie@users.noreply.github.com>
2022-02-17 22:01:21 +0100

    Add files via upload

---
+++ create_map.py
@@ -0,0 +1,28 @@
+import numpy as np
+import create_gaussian_src
+from photutils.datasets import make_noise_image
+
+def create_map(map_size,src_number,src_size,src_std,src_flux_jansky,noise_std):
+    """ This function create a map as a 2D Numpy array, enter map_size,src_number,src_size,src_std,src_flux_jansky,noise_std"""
+
+    #create empty map
+    rectmap=np.zeros((map_size,map_size))
+
+    #create a gaussian source
+    src=create_gaussian_src.create_gaussian_src(src_size,src_std,src_flux_jansky)
+
+    #add src_number source to with random position, but with 2*src_size distant from the boundary.
+    x=np.random.random_integers(low=2*src_size,high=map_size-2*src_size,size=src_number)
+    y=np.random.random_integers(low=2*src_size,high=map_size-2*src_size,size=src_number) #generate random position, note that x and y here represent a matrix like coordinate, so the number of column or absice is y, the number of row or ordonnée is x
+    
+    for i in range(0,len(x)):
+            rectmap[y[i]-(src_size//2):y[i]+((src_size//2)+1),x[i]-(src_size//2):x[i]+((src_size//2)+1)]=src
+    
+    #add gaussian noise
+    noise =  make_noise_image((map_size,map_size), distribution='gaussian', mean=0,stddev=noise_std)
+    rectmap=rectmap+noise
+    catalog=np.transpose((x,y,src_flux_jansky*np.ones_like(x)))#upper pix_center, pix center to index. pix corner ??? plot??    
+    return rectmap
+    
+    
+    
---
+++ sort_signal_to_noise.py
@@ -0,0 +1,42 @@
+import numpy as np
+import numpy.ma as ma
+
+
+# the signal to noise ratio here is the ratio of the average of central kernel and the std of the peripheric kernel, so this function compute the std and later the ratio
+
+
+def create_peripheric_std_map(map,map_size,detection_kernels_size,detection_tools):
+
+    # computing std map
+
+    peripheric_std_map = np.zeros((map_size,map_size))
+    mask=np.array(detection_tools[0][2]==0)
+    for i in range(int(detection_kernels_size[2]/2),map.shape[0]-int(detection_kernels_size[2]/2)):
+        for j in range(int(detection_kernels_size[2]/2),map.shape[1]-int(detection_kernels_size[2]/2)):
+            if (detection_kernels_size[2]/2).is_integer()==False:
+                kernel=map[i-int(detection_kernels_size[2]/2):i+(int(detection_kernels_size[2]/2)+1),j-int(detection_kernels_size[2]/2):j+(int(detection_kernels_size[2]/2)+1)]
+                    
+            else:
+                    
+                kernel=map[i-int(detection_kernels_size[2]/2):i+int(detection_kernels_size[2]/2),j-int(detection_kernels_size[2]/2):j+int(detection_kernels_size[2]/2)]
+                    
+            masked_kernel=ma.masked_array(kernel,mask)
+            std=np.std(masked_kernel)
+            peripheric_std_map[i][j]=std/np.sqrt(detection_kernels_size[2]**2-detection_kernels_size[1]**2)
+    return peripheric_std_map
+
+def compute_signal_to_noise_results(detection_tools,peripheric_std_kernel,detected_src_coor_upper_pixcorner,map):
+    
+    #computing signal to noise ratio
+    signal_to_noise_map = detection_tools[1][0]/peripheric_std_kernel
+    signal_to_noise_list = signal_to_noise_map[detected_src_coor_upper_pixcorner]
+    signal_list = map[detected_src_coor_upper_pixcorner]
+    signal_to_noise_and_coor_list = np.transpose(( np.arange(0,len(detected_src_coor_upper_pixcorner[0])),signal_list,signal_to_noise_list,detected_src_coor_upper_pixcorner[1],detected_src_coor_upper_pixcorner[0] ))
+
+    argsorted_signal_to_noise_index_list = np.flip(np.argsort(signal_to_noise_list))
+
+    sorted_signal_to_noise_and_coor_list = np.transpose(( argsorted_signal_to_noise_index_list, signal_list[argsorted_signal_to_noise_index_list],signal_to_noise_list[argsorted_signal_to_noise_index_list], detected_src_coor_upper_pixcorner[1][argsorted_signal_to_noise_index_list], detected_src_coor_upper_pixcorner[0][argsorted_signal_to_noise_index_list], np.zeros_like(signal_to_noise_list) ))
+
+    return (signal_to_noise_map,signal_to_noise_and_coor_list,sorted_signal_to_noise_and_coor_list)
+
+    
