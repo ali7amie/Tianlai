683991488a4507a55be18144571fe089772dd361
Ali Hamie <92475780+ali7amie@users.noreply.github.com>
2022-08-02 18:18:30 +0200

    Update create_gaussian_src.py

--- create_gaussian_src.py
+++ create_gaussian_src.py
@@ -1,4 +1,4 @@
-import k2jansky
+/* import k2jansky
 import numpy as np
 
 
@@ -12,4 +12,53 @@ def create_gaussian_src(src_size,src_std,src_flux_jansky):
     flux_jansky_of_1k_amplitude=k2jansky.k2jansky('freq',1300*10**6,16.5,1,flux_kelvin_of_1k_amplitude)
     # add the src_flux_jansky factor
     src=(src_flux_jansky/flux_jansky_of_1k_amplitude)*np.exp(-(x-mean_x)**2/(2*src_std**2)) * np.exp(-(y-mean_y)**2/(2*src_std**2))
-    return src
+    return src */
+
+import k2jansky
+import numpy as np
+
+def create_gaussian_src(src_size,src_std,src_flux_jansky,freq):
+
+    ''' This function simulate a single source having gaussian shape, it take the size, the std, the flux in jansky, and the observed frequency. It gives a 2D NumPy array as a source 
+    It use the function k2jansky located in the file k2jansky.py to convert the flux density from kelvin to jansky
+
+    parameters:
+    -------------
+    src_size: int
+                
+    src_std: float
+
+    src_flux_jansky: float
+
+    freq: float
+                          
+       
+    Return:
+    -------  
+    src: tupe (2D NumPy array, float, float)
+         (a single source array, its flux in kelvin, its flux in jansky)
+    '''
+          
+    # create a source with amplitude of 1 k
+    x,y = np.meshgrid(np.linspace(-(src_size//2),src_size//2,src_size),np.linspace(-(src_size//2),src_size//2,src_size))
+    mean_x=0
+    mean_y=0
+    src_1=np.exp(-(x-mean_x)**2/(2*src_std**2)) * np.exp(-(y-mean_y)**2/(2*src_std**2))
+
+    # Compute the flux density of the source having 1 K amplitude
+    flux_kelvin_1=np.sum(src_1)
+
+    # Converting this flux to jansky
+    flux_jansky_1=k2jansky.k2jansky('freq',freq,16.5,1,flux_kelvin_1)
+
+    
+    # create sources having src_flux_jansky Jansky 
+    src=src_flux_jansky * (1 / flux_jansky_1) * np.exp(-(x-mean_x)**2/(2*src_std**2)) * np.exp(-(y-mean_y)**2/(2*src_std**2))
+
+    # compute its flux density in kelvin
+    flux_kelvin= np.sum(src)
+
+    #compute its flux density in jansky
+    flux_jansky=k2jansky.k2jansky('freq',freq,16.5,1,flux_kelvin)
+
+    return (src, flux_kelvin, flux_jansky)
