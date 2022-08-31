8c54d2f8ef61c96d1b139f862bc600bcf075eeb5
Ali Hamie <92475780+ali7amie@users.noreply.github.com>
2022-08-31 11:59:55 +0200

    Update create_gaussian_src.py

--- create_gaussian_src.py
+++ create_gaussian_src.py
@@ -1,9 +1,10 @@
 import k2jansky
 import numpy as np
 
-def create_gaussian_src(src_size,src_std,src_flux_jansky,freq):
+def create_gaussian_src(src_size,src_std,src_flux_jansky,freq,max_baseline):
 
-    ''' This function simulate a single source having gaussian shape, it take the size, the std, the flux in jansky, and the observed frequency. It gives a 2D NumPy array as a source 
+    ''' 
+    This function simulate a single source having gaussian shape, it take the size, the std, the flux in jansky, and the observed frequency. It gives a 2D NumPy array as a source 
     It use the function k2jansky located in the file k2jansky.py to convert the flux density from kelvin to jansky
 
     parameters:
@@ -15,12 +16,14 @@ def create_gaussian_src(src_size,src_std,src_flux_jansky,freq):
     src_flux_jansky: float
 
     freq: float
+
+    max_baseline: float
                           
        
     Return:
     -------  
-    src: tupe (2D NumPy array, float, float)
-         (a single source array, its flux in kelvin, its flux in jansky)
+    src: tuple (2D NumPy array, float, float, float)
+         (a single source array, its flux in kelvin, its flux in jansky, its amplitude in kelvin)
     '''
           
     # create a source with amplitude of 1 k
@@ -33,7 +36,7 @@ def create_gaussian_src(src_size,src_std,src_flux_jansky,freq):
     flux_kelvin_1=np.sum(src_1)
 
     # Converting this flux to jansky
-    flux_jansky_1=k2jansky.k2jansky('freq',freq,16.5,1,flux_kelvin_1)
+    flux_jansky_1=k2jansky.k2jansky(use='freq',freq=freq,max_baseline=max_baseline,beam_surface=274*10**(-7),density_k=flux_kelvin_1)
 
     
     # create sources having src_flux_jansky Jansky 
@@ -42,7 +45,18 @@ def create_gaussian_src(src_size,src_std,src_flux_jansky,freq):
     # compute its flux density in kelvin
     flux_kelvin= np.sum(src)
 
-    #compute its flux density in jansky
-    flux_jansky=k2jansky.k2jansky('freq',freq,16.5,1,flux_kelvin)
+    #compute the amplitude
+    amp_kelvin=np.max(src)
 
-    return (src, flux_kelvin, flux_jansky)
+    #compute its flux density in jansky
+    flux_jansky=k2jansky.k2jansky(use='freq',freq=freq,max_baseline=max_baseline,beam_surface=274*10**(-7),density_k=flux_kelvin)
+
+
+    return (src, flux_kelvin, flux_jansky, amp_kelvin)
+
+
+
+
+
+
+    
